f48f489dee0624975877c173261f18006f4b1d9a
ekachai <ekachai@ibss.co.th>
2017-06-28 14:16:42 +0700

    setup pushnotification

--- MyPushNotification/MyPushNotification/App.xaml.cs
+++ MyPushNotification/MyPushNotification/App.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Com.OneSignal;
+using Com.OneSignal.Abstractions;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
@@ -9,12 +12,73 @@ namespace MyPushNotification
 {
     public partial class App : Application
     {
+        public NotificationReceived mReceivedDelegate;
+        public NotificationOpened mOpenedDelegate;
+
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MyPushNotification.MainPage();
+            SetupPushDelegate();
+            MainPage = new MyPushNotification.MyPushNotificationPage();
+
+            OneSignal.Current.StartInit("e9bfbc59-fa56-48ed-bda0-93002c350641")
+                 .InFocusDisplaying(OSInFocusDisplayOption.None)
+                 .HandleNotificationReceived(mReceivedDelegate)
+                 .HandleNotificationOpened(mOpenedDelegate)
+                 .EndInit();
+
+
+            OneSignal.Current.IdsAvailable((playerID, pushToken) =>
+            {
+                MyPushNotificationPage page = MainPage as MyPushNotificationPage;
+                page.UpdateOneSignalID(playerID);
+            });
         }
+
+        void SetupPushDelegate()
+        {
+            mReceivedDelegate = delegate (OSNotification notification)
+            {
+                try
+                {
+                    Debug.WriteLine("OneSignal Notification Received:\nMessage: {0}", notification.payload.body);
+                    Dictionary<string, object> additionalData = notification.payload.additionalData;
+                    var newmessage = notification.payload.title + ": " + notification.payload.body;
+                    MyPushNotificationPage page = MainPage as MyPushNotificationPage;
+                    page.UpdateNewMessage(newmessage);
+                    if (additionalData.Count > 0)
+                        Debug.WriteLine("additionalData: {0}", additionalData);
+                }
+                catch (System.Exception e)
+                {
+                    Debug.WriteLine(e.StackTrace);
+                }
+            };
+
+            // Notification Opened Delegate
+            mOpenedDelegate = delegate (OSNotificationOpenedResult result)
+            {
+                try
+                {
+                    Debug.WriteLine("OneSignal Notification opened:\nMessage: {0}", result.notification.payload.body);
+                    Dictionary<string, object> additionalData = result.notification.payload.additionalData;
+                    if (additionalData.Count > 0)
+                        Debug.WriteLine("additionalData: {0}", additionalData);
+
+
+                    List<Dictionary<string, object>> actionButtons = result.notification.payload.actionButtons;
+                    if (actionButtons.Count > 0)
+                        Debug.WriteLine("actionButtons: {0}", actionButtons);
+                }
+                catch (System.Exception e)
+                {
+                    Debug.WriteLine(e.StackTrace);
+                }
+            };
+
+        }
+
 
         protected override void OnStart()
         {
---
+++ MyPushNotification/MyPushNotification/MyPushNotificationPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyPushNotification
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyPushNotificationPage : ContentPage
+    {
+
+        public MyPushNotificationPage()
+        {
+            InitializeComponent();
+        }
+
+        public void UpdateOneSignalID(string id)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                
+                mOneSignalIDLabel.Text = id;
+            });
+
+        }
+
+        public void UpdateNewMessage(string message)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                mNewMessageLabel.Text = message;
+            });
+        }
+    }
+}
